90f5d84f71709a32089b562efe6df58860c69f03
Gabriel Mendes <gmendescamargodebarros@gmail.com>
2022-11-20 21:25:22 -0300

    crud pastorais

--- src/pages/ChurchMinistry/index.tsx
+++ src/pages/ChurchMinistry/index.tsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect, useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 // import { Link } from "react-router-dom";
 import { apiLocal } from "../../services/api";
 import { Title } from "../../components/Title";
 import { AuthContext } from "../../contexts/auth";
 import { Form } from "./styles";
+
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+
 
 interface ChurchMinistry {
   id: string;
@@ -14,9 +20,11 @@ interface ChurchMinistry {
 }
 
 export const ChurchMinistry: React.FC = () => {
-  const { repository } = useParams();
+  const navigate = useNavigate();
+
+  const { id } = useParams();
   const { user } = useContext(AuthContext);
-  
+
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [ministry, setChurchMinistry] = useState<ChurchMinistry>(
@@ -24,52 +32,81 @@ export const ChurchMinistry: React.FC = () => {
   );
 
   async function getMinistry(id: string) {
-    const ministry = await apiLocal.get<ChurchMinistry>(`/pastoral/${id}`);
-    setChurchMinistry(ministry.data);
+    try {
+      const { data } = await apiLocal.get<ChurchMinistry>(`/pastoral/${id}`);
+      setChurchMinistry(data);
+      setName(data.name);
+      setDescription(data.description);
+    } catch (error) {
+      Swal.fire('Oops', 'Não encontramos os dados da sua pastoral', 'info')
+      return navigate('/')
+    }
   }
-  
-  if (!repository) {
-    return (<>eRRO</>);
-  }
-  
+
   async function hadleSubmit(
     event: React.FormEvent<HTMLFormElement>
-    ): Promise<void> {
-      event.preventDefault();
-      
-      if (!repository || !user) {
-        return;
+  ): Promise<void> {
+    event.preventDefault();
+    if (!id || !user) {
+      return;
+    }
+    const churchMinistry = { name, description, creator_id: user.id };
+    const ministryUpdate = await apiLocal.put<ChurchMinistry>(
+      `/pastoral/${id}`,
+      churchMinistry
+    );
+
+    setChurchMinistry(ministryUpdate.data);
+  }
+
+  async function deleteChurchMinistryOnClick() {
+    MySwal.fire({
+      showDenyButton: true,
+      title: <h2>Aviso</h2>,
+      html: (
+        <p>
+          Tem certeza que deseja excluir a pastoral? 
+          <br/>Todos os dados da pastoral
+          serão permanentemente deletados.
+        </p>
+      ),
+      denyButtonText: "Excluir",
+      confirmButtonText: "Cancelar",
+    }).then(async (result: any) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isDenied) {
+        await apiLocal.delete(`/pastoral/${id}`)
+        Swal.fire('Saved!', '', 'success')
+        return navigate('/');
       }
-      
-      const churchMinistry = { name, description, creator_id: user.id };
-      
-      const ministry = await apiLocal.put<ChurchMinistry>(
-        `/pastoral${repository}`,
-        churchMinistry
-        );
-      }
+    });
+  }
 
   useEffect(() => {
-    console.log(repository, user)
-    getMinistry(repository);
-    console.log('entrou')
-  }, []);
-  
+    if (!id) {
+      return navigate('/');
+    }
+
+    getMinistry(id);
+  }, [id]);
+
   return (
     <>
       <Title text={"Pastoral"} />
       <Form onSubmit={hadleSubmit}>
         <input
-          value={ministry.name}
           onChange={(e) => setName(e.target.value)}
-          placeholder="Nome da pastoral"
+          placeholder={ministry.name}
         />
         <input
-          value={ministry.description}
           onChange={(e) => setDescription(e.target.value)}
-          placeholder="Descrição da pastoral"
+          placeholder={ministry.description}
         />
         <button type="submit"> Atualizar pastoral </button>
+        <button type="button" onClick={deleteChurchMinistryOnClick}>
+          {" "}
+          Excluir Pastoral
+        </button>
       </Form>
     </>
   );
